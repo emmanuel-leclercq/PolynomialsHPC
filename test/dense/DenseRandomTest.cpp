063a653205f915a613b4eb20188073d71b819d5a
emmanuel-leclercq <68131409+Numag@users.noreply.github.com>
2024-03-23 15:33:03 +0100

    documenting

--- test/dense/DenseRandomTest.cpp
+++ test/dense/DenseRandomTest.cpp
@@ -8,6 +8,10 @@
 namespace {
     TEST(RandomTest, Unif) {
         auto P = generateRandomIntPolynomial(100, 0, 1);
+        /*
+         * This test is not ideal: there is a non-zero
+         * (although very low) probability that the polynomial is zero
+         */
     EXPECT_NE(P.degree(),0);
 }
     TEST(RandomTest, CustomGenerator) {
