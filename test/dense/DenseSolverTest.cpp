68bcbc56fd89771d706b69b4d67de6888cc5a7d9
emmanuel-leclercq <68131409+Numag@users.noreply.github.com>
2024-04-24 14:25:19 +0200

    Roots will be ordered (when real) for easy comparison

--- test/dense/DenseSolverTest.cpp
+++ test/dense/DenseSolverTest.cpp
@@ -7,11 +7,15 @@
 
 namespace {
     TEST(SolverTest, DegreeOne) {
-        Polynomial<int> p1{{6, 1}};
+        std::vector<int> v1 = {1, 6};
+        Polynomial<int> p1(v1, true);
+        std::cout << p1 << std::endl;
+        std::cout<<"p[0]="<<p1[0]<<" p[1]="<<p1[1]<<" p[2]="<<p1[2]<<std::endl;
         Polynomial<double> p2({2, 3}, true);
 
-        std::vector<double> Rootsp1 = solveRoots<int, double>(p1);
-        std::vector<double> Rootsp2 = solveRoots<double, double>(p2);
+        std::vector<int> Rootsp1 = solveRoots<int, int>(p1);
+        EXPECT_EQ(Rootsp1, v1);
+//        std::vector<double> Rootsp2 = solveRoots<double, double>(p2);
 
 
     }
