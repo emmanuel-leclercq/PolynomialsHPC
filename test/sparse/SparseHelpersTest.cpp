19d4157c8f4566960213476b6c2eb7e68c16fb5c
Emmanuel Leclercq <manug677@gmail.com>
2024-06-28 18:45:44 +0200

    reorder and adjust tests done, add too, but it doesn't handle existing degrees pushed in yet

--- test/sparse/SparseHelpersTest.cpp
+++ test/sparse/SparseHelpersTest.cpp
@@ -5,23 +5,27 @@
 
 #include "gtest/gtest.h"
 #include <list>
+#include <vector>
 
 using namespace Polynomial;
 namespace {
     TEST(HelpersTest, Trivial) {
         //reorder
-        Sparse<int> p1(std::list<int>{6, 3, 0, 1, 5});
-        /*
-        Sparse<int> p2(std::vector<std::pair<int, int>>{
-                {0, 3},
-                {1, 1},
-                {3, 1},
-                {5, 1},
-                {6, 1}});
+        Sparse p1(std::list<int>{6, 3, 0, 1, 5});
 
+        std::vector<std::pair<int, int>> pv({{4, 5},
+                                             {3, 1},
+                                             {2, 0},
+                                             {1, 3},
+                                             {0, 6},});
+        Sparse p2(pv);
+        EXPECT_EQ(p1, p2); // implicit call to .reorder() and .adjust() removing null coeffs
+        Monomial<int> b({8, 8});
+        p1.add(b);
         p1.reorder();
-        p2.reorder();
-        EXPECT_EQ(p1, p2);
-         */
+        EXPECT_FALSE(p1 == p2);
+        EXPECT_EQ(p1[8], b);
+
+
     }
 }
